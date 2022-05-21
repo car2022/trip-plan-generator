81d4c3ea55ef168101ee1b6cc86d0b81c51ca2b8
t.Yuirino <t.Yurino>
2022-05-21 18:12:43 +0900

    環境構築
    vue-cliを用いてvue3とTypescript導入

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <TripComponent />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import TripComponent from "./components/trip_component/index.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    TripComponent,
+  },
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  width: 100vw;
+  height: 100vh;
+}
+</style>
---
+++ src/components/trip_component/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="rootContainer">
+    <div class="title">旅行先生成サイト</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "TripComponent",
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped src="./index.css" />
